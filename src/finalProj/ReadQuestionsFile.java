809bed900d4649643afc5dbd6c826f921d4b28b1
Eric Hu <62254141+ericwuhu@users.noreply.github.com>
2020-04-22 14:10:56 -0700

    Update ReadQuestionsFile
    Started the read questions file

--- src/finalProj/ReadQuestionsFile.java
+++ src/finalProj/ReadQuestionsFile.java
@@ -1,17 +1,68 @@
 package finalProj;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Retrieve questions and answers from file and adds them to questions
  * @author ericw
  *
  */
-public class ReadQuestionsFile implements Questions {
+public class ReadQuestionsFile implements Questions{
 	
-	public String setQuestion() {
+	ArrayList<Questions> questions = new ArrayList<Questions>();
+	
+	/**
+	 * If no questions are asked, refers to this method
+	 * @throws FileNotFoundException
+	 */
+	public void readFile() throws FileNotFoundException {
+		readFile("quiz.qs");
+	}
+	
+	/**
+	 * Method used to find a specific file of questions
+	 * @param fileName
+	 * @throws FileNotFoundException
+	 */
+	public void readFile(String fileName) throws FileNotFoundException
+	{
+		Scanner scan = new Scanner(new File(fileName + ".txt"));
+		
+		int questionNumber = 0;
+		String question;
+		String answer;
+		
+		// While loop used for while file has a next line
+		while(scan.hasNextLine()) {
+			String line = scan.nextLine(); // line is the entire line
+			
+			Scanner lineScan = new Scanner(line); // Scans the line
+			
+			// Setting values to numbers
+			questionNumber = lineScan.nextInt();
+			question = lineScan.next();
+			answer = lineScan.next();
+			
+			questions.add()
+		}
+	}
+	
+	public void setQuestion() {
 		
 	}
 	
-	public String setCorrectAnswer() {
+	public void setAnswer() {
+		
+	}
+	
+	public String getQuestion() {
+		
+	}
+	
+	public String getCorrectAnswer() {
 		
 	}
 }
