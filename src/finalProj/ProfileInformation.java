b80a476bca8a2799a5d7fde06c8b9e8e79fe7f08
ianduffield <ianduffield@brandeis.edu>
2020-04-24 16:20:53 -0400

    made profileinformation an abstract class

--- src/finalProj/ProfileInformation.java
+++ src/finalProj/ProfileInformation.java
@@ -2,11 +2,11 @@ package finalProj;
 
 /**
  * Find user's profile and retrieves information from their file
- * @author ericw
+ * @author ianduffield
  *
  */
 
 // I believe that we wanted to be able to keep track of a users information
-public class ProfileInformation {
-
+public abstract class ProfileInformation{
+	
 }
