60b94cc2a36491e480a0012704abab17a83a1c8e
ianduffield <ianduffield@brandeis.edu>
2020-04-24 16:47:39 -0400

    user interface changes

--- src/finalProj/UserInterface.java
+++ src/finalProj/UserInterface.java
@@ -1,15 +1,38 @@
 package finalProj;
-
+import java.util.Scanner;
 /**
+ *Constructor for ProfileInformation needs to have username, a String username is passed
+ *
  * Information is displayed and retrieved from user
- * @author ericw
+ * 
+ *
+ * @author ianduffield
  *
  */
-public class UserInterface {
+public class UserInterface{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		System.out.println("Welcome to the Platinum Trivia App!");
+		System.out.println("Please enter your username as one String: ");
+		Scanner input = new Scanner(System.in);
+		String username = input.nextLine();
+		ProfileInformation currentUser = new ProfileInformation(username);
+		while(!input.nextLine().equals("STOP")){
+			currentUser.getQuestion();
+			String[] answer = input.nextLine().split(" ");
+			int prevScore = currentUser.getCorrect();
+			for(int i = 0; i < answer.length; i++){
+				for(int j = 0; j < currentUser.getAnswer().length; j++){
+					if(answer[i].toLowerCase().equals(currentUser.getAnswer(j))){
+						currentUser.setCorrect(1);
+						System.out.println("That's right! The correct answer was " + currentUser.getAnswer());
+						break;
+					}
+				}
+			} if (prevScore != currentUser.getCorrect()){
+				System.out.println("Sorry, the right answer was " + currentUser.getAnswer());
+			}
+		}
 	}
 
 }
