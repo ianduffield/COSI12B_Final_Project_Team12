39349439d0ed47ac7e2971c509e679c55e224345
Ghazalhashemi <57968331+Ghazalhashemi@users.noreply.github.com>
2020-04-24 17:36:59 -0400

    change

--- src/finalProj/ProfileInformation.java
+++ src/finalProj/ProfileInformation.java
@@ -9,24 +9,32 @@ import java.io.*;
 
 // I believe that we wanted to be able to keep track of a users information
 public class ProfileInformation{
-	private String username;
-	private Questions question;
-	
-	
-	public void ProfileInformation (String username, Questions question){
-		this.username = username;
-		this.question = question;;
+//	private String username;
+//	private Questions question;
+//	
+//	
+//	public void ProfileInformation (String username, Questions question){
+//		this.username = username;
+//		this.question = question;;
 		
-	}
+//	}
 	public static void main (String[] args) throws IOException{
-		String username = "hi"+".txt";
+		String username1 = "test";
+		String username = username1+".txt";
 		File file = new File ("hi.txt");
 		FileWriter write = new FileWriter(file);
 		PrintWriter input = new PrintWriter(write);
 		
-		input.println("TEST TEST");
-		input.println("TEST2 TEST2");
+		input.print(35);
+		input.print(45);
+
+		input.print("       ");
 		input.close();
+
+		
+//		input.print(question.getId());
+//		input.print("       ");
+//		input.close();
 
 	}
 	
