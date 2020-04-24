45196cb0f6c67a6c2860163590228e08fa92fb00
Ghazalhashemi <57968331+Ghazalhashemi@users.noreply.github.com>
2020-04-24 17:23:31 -0400

    made the profileinfo class to make new .txt files

--- src/finalProj/ProfileInformation.java
+++ src/finalProj/ProfileInformation.java
@@ -1,4 +1,5 @@
 package finalProj;
+import java.io.*;
 
 /**
  * Find user's profile and retrieves information from their file
@@ -10,7 +11,24 @@ package finalProj;
 public class ProfileInformation{
 	private String username;
 	private Questions question;
-	private 
+	
+	
+	public void ProfileInformation (String username, Questions question){
+		this.username = username;
+		this.question = question;
+		
+	}
+	public static void main (String[] args) throws IOException{
+		String username = "hi"+".txt";
+		File file = new File ("hi.txt");
+		FileWriter write = new FileWriter(file);
+		PrintWriter input = new PrintWriter(write);
+		
+		input.println("TEST TEST");
+		input.println("TEST2 TEST2");
+		input.close();
+
+	}
 	
 	
 }
