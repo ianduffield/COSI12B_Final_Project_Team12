e80718fea9eeb503443e1bf625c47558bf7c323a
WylliumHummh <51054277+WylliumHummh@users.noreply.github.com>
2020-04-27 19:37:34 -0400

    Update UserInterface.java

--- src/finalProj/UserInterface.java
+++ src/finalProj/UserInterface.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
-import java.util.Random; 
+import java.util.Random;
 import java.util.List;
 import java.util.Queue;
 
@@ -13,13 +13,13 @@ import java.util.Queue;
  *Constructor for ProfileInformation needs to have username, a String username is passed
  *
  * Information is displayed and retrieved from user
- * 
+ *
  *
  * @author ianduffield
  *
  */
 public class UserInterface{
-	
+
 	public static void main(String[] args) throws IOException{
 		UserInterface user = new UserInterface();
 		System.out.println("Welcome to the Platinum Trivia App!");
@@ -57,10 +57,12 @@ public class UserInterface{
 			}
 		} System.out.println("Hope you had fun! Your score for this session was " + score);
 		currentUser.Score(score);
-	} 
+	}
 	public Queue<Integer> randomQueue(ReadQFiles quizzes) throws FileNotFoundException{
-		// Creates a queue of numbers put in a random order based on the quiz length.
-		// You don't have to worry about repeat questions with these. 
+		/* Creates a queue of numbers put in a random order based on the quiz length.
+		* @author William
+		* You don't have to worry about repeat questions with these.
+		*/
 		 Queue<Integer> q = new LinkedList<>();
 		 int quizSize = quizzes.getQuestions().size(); //This is meant to be the number of questions in the quiz.
 		 int[] questionList = new int[quizSize];
@@ -69,7 +71,7 @@ public class UserInterface{
 			 questionList[quest] = quest;
 		 }
 		 Random rng = new Random();
-		 // This loop is responsible for randomizing the order.	
+		 // This loop is responsible for randomizing the order.
 			for (int i = 0; i < questionList.length; i++) {
 				int randomIndex = rng.nextInt(questionList.length);
 				int temp = questionList[randomIndex];
@@ -80,9 +82,9 @@ public class UserInterface{
 			 q.add(questionList[question]);
 		 } return q;
 		 //q.pop() can be used to remove a question that is already done.
- 	// The interface uses this method to call and generate a new question using the current queue number available. 
+ 	// The interface uses this method to call and generate a new question using the current queue number available.
 	}
-	
+
 	public int checkAnswer(String[] answer, ProfileInformation currentUser, ReadQFiles quizzes, Question currentQ, int score) throws IOException{
 		for(int i = 0; i < answer.length; i++){
 			for(int j = 0; j < currentQ.getAnswerList().size(); j++){
